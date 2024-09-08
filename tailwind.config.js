239de73b83b6493d545501d6e18fd9db4c26d0c1
coder-boy-laju <mdlaju43@gmail.com>
2024-09-08 17:21:47 +0600

    Navbar done with Responsive

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,22 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
+const defaultTheme = require("tailwindcss/defaultTheme");
 
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./index.html"],
   theme: {
     extend: {
-fontFamily:{
-  manrope: [ "Manrope", ...defaultTheme.fontFamily.sans]
-}
-
+      fontFamily: {
+        manrope: ["Manrope", ...defaultTheme.fontFamily.sans],
+      },
+      colors: {
+        "primary": "#FF42A5",
+        "gradient-1": "#F948B2",
+        "gradient-2": "#8758F1",
+        "blue": "#4EC5F7",
+        "dim-gray": "#534C64",
+        "deep-navy-blue": "#0B0223",
+      },
     },
   },
   plugins: [],
-}
-
+};
